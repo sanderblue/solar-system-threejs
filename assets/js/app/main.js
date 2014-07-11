8a0a26b318b4f523b7f5b9a46a08dd082e815e18
Sander Blue <william.a.blue@gmail.com>
2014-07-10 21:18:20 -0500

    and boom we have a visually working camera orbit control for the UI

--- assets/js/app/main.js
+++ assets/js/app/main.js
@@ -115,6 +115,37 @@ define(
             init: function() {
                 UIController.init();
 
+                function rotateAnnotationCropper(offsetSelector, xCoordinate, yCoordinate, cropper){
+                    //alert(offsetSelector.left);
+
+                    var x = xCoordinate - offsetSelector.offset().left - offsetSelector.width()/2;
+                    var y = -1*(yCoordinate - offsetSelector.offset().top - offsetSelector.height()/2);
+                    var theta = Math.atan2(y,x)*(180/Math.PI);
+
+
+                    var cssDegs = convertThetaToCssDegs(theta);
+                    var rotate = 'rotate(' +cssDegs + 'deg)';
+                    cropper.css({'-moz-transform': rotate, 'transform' : rotate, '-webkit-transform': rotate, '-ms-transform': rotate});
+                    $('body').on('mouseup', function(event){ $('body').unbind('mousemove')});
+
+                }
+
+                function convertThetaToCssDegs(theta){
+                    var cssDegs = 90 - theta;
+                    return cssDegs;
+                }
+
+                console.log($('#marker'));
+
+                $(document).ready(function(){
+                    $('#marker').on('mousedown', function(){
+                        $('body').on('mousemove', function(event){
+                            console.log(event)
+                            rotateAnnotationCropper($('#innerCircle').parent(), event.pageX,event.pageY, $('#marker'));
+                        });
+                    });
+                });
+
                 if (App.config.buildEnabled) {
                     $.when(Initializer.init()).done(function() {
                         MainController.animate();
