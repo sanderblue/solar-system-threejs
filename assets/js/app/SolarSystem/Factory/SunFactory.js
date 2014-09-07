cae8ec5179128e01142ef2a86538a12ac3e6dd2d
Sander Blue <william.a.blue@gmail.com>
2014-09-07 18:25:26 -0500

    wtf

--- assets/js/app/SolarSystem/Factory/SunFactory.js
+++ assets/js/app/SolarSystem/Factory/SunFactory.js
@@ -13,7 +13,7 @@ define(
          */
         var SunFactory = {
             getTexture: function() {
-                return new THREE.ImageUtils.loadTexture('/textures/sun_detailed.png');
+                return new THREE.ImageUtils.loadTexture('/textures/sun_detasdfsdfsdfiled.png');
             },
 
             build: function() {
