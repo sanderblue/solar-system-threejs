1c20cc72e4e1331f7816509d5ebe9dd93eaf4ed8
Sander Blue <william.a.blue@gmail.com>
2014-09-07 22:51:33 -0500

    adjustments for camera zoom and orbit control

--- assets/js/app/App.js
+++ assets/js/app/App.js
@@ -6,7 +6,7 @@ define(function() {
             SunFactoryEnabled: true,
             PlanetFactoryEnabled: true,
             MoonFactoryEnabled: true,
-            AsteroidBeltFactoryEnabled: true,
+            AsteroidBeltFactoryEnabled: false,
             RingFactoryEnabled: true,
             OrbitFactoryEnabled: true,
             SolarSystemFactoryEnabled: true,
