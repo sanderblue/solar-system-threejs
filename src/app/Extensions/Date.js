cbc9486dc44bd8a469892e6fc1bf73347e302fff
Sander Blue <william.a.blue@gmail.com>
2015-09-26 10:06:17 -0500

    update date extension to allow precision for return of day or year as decimal

--- src/app/Extensions/Date.js
+++ src/app/Extensions/Date.js
@@ -1,46 +1,47 @@
 define(function() {
+  'use strict';
 
-    /**
-     * Date.js
-     *
-     * Extends the JavaScript date object
-     */
+  /**
+   * Gets the current day of the year.
+   *
+   * @return {Integer}
+   */
+  Date.prototype.getDOY = function() {
+      var onejan = new Date(this.getFullYear(), 0, 1);
 
+      return Math.ceil((this - onejan) / 86400000);
+  };
 
-    /**
-     * Gets the current day of the year.
-     *
-     * @return integer
-     */
-    Date.prototype.getDOY = function() {
-        var onejan = new Date(this.getFullYear(), 0, 1);
+  /**
+   * Gets the current local time and returns it in decimal format.
+   * Uses ES6 `Number.parseInt()` instead of the global `parseInt()`.
+   *
+   * @return {Float}
+   */
+  Date.prototype.timeStringToDecimal = function () {
+      var timeString   = this.toTimeString(),
+          hoursMinutes = timeString.split(/[.:]/),
+          hours        = Number.parseInt(hoursMinutes[0], 10),
+          minutes      = hoursMinutes[1] ? Number.parseInt(hoursMinutes[1], 10) : 0
+      ;
 
-        return Math.ceil((this - onejan) / 86400000);
-    };
+      return hours + minutes / 60;
+  };
 
-    /**
-     * Gets the current local time and returns it in decimal format.
-     *
-     * @return float
-     */
-    Date.prototype.timeStringToDecimal = function () {
-        var timeString   = this.toTimeString(),
-            hoursMinutes = timeString.split(/[.:]/),
-            hours        = parseInt(hoursMinutes[0], 10),
-            minutes      = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0
-        ;
+  /**
+   * Gets the current day of the year with the current time of day and
+   * returns it in decimal format. Uses ES6 `Number.parseFloat()` to
+   * return an floating point number from a number string.
+   *
+   * @param  {Integer} toFixed
+   * @return {Float}
+   */
+  Date.prototype.getDOYwithTimeAsDecimal = function(toFixed) {
+    var float = this.getDOY() + this.timeStringToDecimal() / 24;
 
-        return hours + minutes / 60;
-    };
-
-    /**
-     * Gets the current day of the year with the current time of day and
-     * returns it in decimal format.
-     *
-     * @return integer
-     */
-    Date.prototype.getDOYwithTimeAsDecimal = function() {
-       return this.getDOY() + this.timeStringToDecimal() / 24;
-    };
-
+    return toFixed && toFixed > 0
+      ? Number.parseFloat(float.toFixed(toFixed))
+      : float
+    ;
+  };
 });
