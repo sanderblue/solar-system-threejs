a6f52ea20521ceb6e5c3a73f77f46f1a3c47efa3
Sander Blue <william.a.blue@gmail.com>
2016-03-09 21:10:25 -0600

    better detaching and attaching camera when traveling from object to object

--- src/app/Controllers/TravelController.js
+++ src/app/Controllers/TravelController.js
@@ -3,65 +3,69 @@ define(function() {
 
     class TravelController {
         constructor(scene) {
-            this.scene = scene;
-            this.camera = this.scene.camera;
-            this.travelStartEvent = new CustomEvent('travelStart');
-            this.travelCompleteEvent = new CustomEvent('travelComplete');
-            this.targetPosition = new THREE.Vector3();
+          this.scene = scene;
+          this.camera = this.scene.camera;
+          this.travelStartEvent = new CustomEvent('travelStart');
+          this.travelCompleteEvent = new CustomEvent('travelComplete');
+          this.targetPosition = new THREE.Vector3();
         }
 
         travelToObject(currentPosition, targetObject) {
-            console.debug('Target Object:', targetObject);
+          console.debug('Target Object:', targetObject);
 
-            document.dispatchEvent(this.travelStartEvent);
+          var travelDuration = 5000; // milliseconds
 
-            var currentCameraPosition = this.camera.position;
-            var targetPosition = targetObject.threeObject.position;
-            var travelDuration = 6000; // milliseconds
+          document.dispatchEvent(this.travelStartEvent);
 
-            targetObject.orbitCentroid.add(this.camera);
-            this.camera.position.x = currentCameraPosition.x;
-            this.camera.position.y = currentCameraPosition.y;
-            this.camera.position.z = currentCameraPosition.z;
+          THREE.SceneUtils.detach(this.camera, this.camera.parent, this.scene);
+          THREE.SceneUtils.attach(this.camera, this.scene, targetObject.orbitCentroid);
 
-            this.camera.up.set(0, 0, 1);
-            this.camera.lookAt(targetObject.threeObject.position);
+          // targetObject.orbitCentroid.updateMatrixWorld();
 
-            // var cameraTween = new TWEEN.Tween(this.camera.position)
-            //     .to(targetObject.core.position, travelDuration)
-            //     .easing(TWEEN.Easing.Cubic.InOut)
-            //     .onUpdate(function(currentAnimationPosition) {
-            //         // Follow the target for a smooth transition from "flight" to "orbit".
-            //         // this.y = targetObject.threeObject.position.y;
-            //         // this.camera.lookAt(targetObject.threeObject.position);
-            //         // this.targetPosition = targetObject.threeObject.position;
-            //     }.bind(this))
-            //     .onComplete(this.handleComplete.bind(this, targetObject))
-            //     .start()
-            // ;
+          targetObject.core.add(new THREE.AxisHelper(26));
+
+          // targetObject.orbitCentroid.add(this.camera);
+          this.camera.up.set(0, 0, 1);
+          this.camera.lookAt(targetObject.threeObject.position);
+
+          var takeOff = this.prepareForTravel(targetObject);
+
+          takeOff.start().onComplete(()=> {
+            console.debug('Take off complete...');
+
+            var cameraTween = new TWEEN.Tween(this.camera.position)
+              .to(targetObject.core.position, travelDuration)
+              .easing(TWEEN.Easing.Cubic.InOut)
+              .onUpdate(function(currentAnimationPosition) {
+                  this.camera.lookAt(targetObject.threeObject.position);
+              }.bind(this))
+              .onComplete(this.handleComplete.bind(this, targetObject))
+              .start()
+            ;
+          });
         }
 
         prepareForTravel(targetObject) {
           var self = this;
 
+          console.debug('prepareForTravel', targetObject);
+          console.debug('Camera', this.camera);
+
           return new TWEEN.Tween(this.camera.position)
             .to({
               x: this.camera.position.x,
               y: this.camera.position.y,
-              z: this.camera.position.z + 500
-            },
-              liftOffDuration
-            )
+              z: this.camera.position.z + 8000
+            }, 3000)
             .easing(TWEEN.Easing.Cubic.InOut)
-            .onUpdate(()=> {
-              this.camera.lookAt(targetObject.position);
-            })
-            .start()
+            .onUpdate(function(currentAnimationPosition) {
+                this.camera.lookAt(targetObject.threeObject.position);
+            }.bind(this))
           ;
         }
 
         calculateTravelToPoint(targetObject) {
-            var posX = targetObject.threeDiameter + 1.5 + (targetObject.threeDiameter / 2.5);
+            var posX = targetObject.threeDiameter + 1.5 + (targetObject.threeDiameter / 2);
 
             return {
                 x: posX,
@@ -71,19 +75,24 @@ define(function() {
         }
 
         handleComplete(targetObject) {
-            this.camera.lookAt(new THREE.Vector3());
+            // this.camera.lookAt(new THREE.Vector3());
             var cameraDistanceFromTarget = targetObject.threeDiameter + 1.5 + (targetObject.threeDiameter / 2);
             var endPoint = this.calculateTravelToPoint(targetObject);
 
             // console.debug('Camera Position:', this.camera.position);
             // console.debug('Target Position:', targetObject.threeObject.position);
-            // console.debug('End Point: ', endPoint);
+            console.debug('targetObject.core.position: ', targetObject.core.position);
+
+            targetObject.core.add(this.camera);
 
             this.camera.position.x = endPoint.x; // newPosX; // zoom
             this.camera.position.y = endPoint.y; // newPosY; // vertical positioning of the camera
             this.camera.position.z = endPoint.z; // 0;       // this is really the y-axis in terms of plan view
 
-            targetObject.core.add(this.camera);
+            targetObject.orbitCentroid.updateMatrixWorld();
+            this.camera.lookAt(new THREE.Vector3());
+
+            console.debug('this.camera.position: ', this.camera.position);
 
             // console.debug(
             //   'Distance To Target',
