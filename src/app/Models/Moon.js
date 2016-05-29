6d883ba89241894aeecfc1fd264d05c6fa9f3e40
Sander Blue <william.a.blue@gmail.com>
2016-05-29 12:06:43 -0500

    correcting moon orbit inclinations - they should all be based on the ecliptic plane

--- src/app/Models/Moon.js
+++ src/app/Models/Moon.js
@@ -156,10 +156,6 @@ function(Constants, CelestialObject, Orbit) {
 
     buildFullObject3D() {
       this._orbitLine = new Orbit(this, this._orbitColorDefault);
-      this._orbitCentroid.rotation.y += this._parentData.axialTilt * Constants.degreesToRadiansRatio; // double check this
-
-      // this._threeObject.rotation.x = 90 * Constants.degreesToRadiansRatio;
-      // this._threeObject.rotation.z = this._axialTilt * Constants.degreesToRadiansRatio;
 
       this._orbitCentroid.add(
         this._threeObject,
