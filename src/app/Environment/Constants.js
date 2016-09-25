9af3eb3ce112a312be148d9c41bebf418b153b81
Sander Blue <william.a.blue@gmail.com>
2016-09-25 13:17:51 -0500

    update celestial scale

--- src/app/Environment/Constants.js
+++ src/app/Environment/Constants.js
@@ -7,7 +7,7 @@ define(function() {
      * This object contains a number of mathematical and physical constants.
      */
     const UNIVERSE_SCALE = Math.pow(10, -4.2); // 4.2
-    const CELESTIAL_SCALE = Math.pow(10, -3.75); // 3.9
+    const CELESTIAL_SCALE = Math.pow(10, -3.8); // 3.9
     const ORBIT_SCALE = UNIVERSE_SCALE;
     const DEGREES_TO_RADIANS_RATIO = 0.0174532925;
     const RADIANS_TO_DEGREES_RATIO = 57.2957795;
