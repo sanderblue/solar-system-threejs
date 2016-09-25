532519851346e1b23e6daccf8c75b7a6e96ee2ec
Sander Blue <william.a.blue@gmail.com>
2016-09-25 13:32:28 -0500

    use relative path for getting the data for now

--- src/app/Modules/Loader.js
+++ src/app/Modules/Loader.js
@@ -52,7 +52,7 @@ function(
   var templateLoader = new TemplateLoader();
   var dataRequest = new HttpRequest(
     'GET',
-    window.location.origin + '/src/data/solarsystem.json',
+    '../../src/data/solarsystem.json',
     true
   );
 
